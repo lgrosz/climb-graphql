0269584b1976b3f60ab1188d4c3357fa2cbc386e
Logan Grosz <logan.grosz@gmail.com>
2024-10-30 23:04:55 -0600

    refactor(graphql): remove use of climb-db
    I will be migrating to a backend using [climb-pg](https://github.com/lgrosz/climb-pg).

--- climb-graphql/src/main.rs
+++ climb-graphql/src/main.rs
@@ -1,5 +1,4 @@
 mod schema;
-mod queries;
 
 use async_graphql::{http::GraphiQLSource, EmptySubscription, Schema};
 use async_graphql_axum::GraphQL;
@@ -12,25 +11,13 @@ use schema::MutationRoot;
 use tokio::net::TcpListener;
 use crate::schema::QueryRoot;
 
-use diesel::pg::PgConnection;
-use diesel::r2d2::{self, ConnectionManager};
-use std::env;
-
 async fn graphiql() -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint("/graphql").finish())
 }
 
 #[tokio::main]
 async fn main() {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
-
     let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
-        .data(pool.clone())
         .finish();
 
     let app = Router::new().route("/graphql", get(graphiql).post_service(GraphQL::new(schema)));
