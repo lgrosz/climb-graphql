80683c5666a24001c8bb37d7fb7cd9baa20c8438
Logan Grosz <logan.grosz@gmail.com>
2025-02-21 16:01:56 -0700

    test: add vermin-grade scalar tests
    These tests cover all lines (except the else condition of
    `captures.get(2)` since that seemingly cannot be reached with the regex
    pattern).

--- src/schema/vermin_grade.rs
+++ src/schema/vermin_grade.rs
@@ -25,3 +25,56 @@ impl ScalarType for VerminGrade {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_good() {
+        let values = [
+            Value::String("V0".into()),
+            Value::String("V255".into()),
+            Value::String("v0".into()),
+            Value::String("v255".into()),
+            Value::String("0".into()),
+            Value::String("255".into()),
+        ];
+
+        let expected = [
+            0,
+            255,
+            0,
+            255,
+            0,
+            255,
+        ];
+
+        for (v, e) in values.iter().zip(expected.iter()) {
+            assert_eq!(VerminGrade::parse(v.clone()).unwrap().0, *e);
+        }
+    }
+
+    #[test]
+    fn parse_bad() {
+        let values = [
+            Value::Number(0.into()), // only string-values accepted
+            Value::String("5.10".into()), // doesn't match regex
+            Value::String("V256".into()), // cannot be contained in a u8
+        ];
+
+        for v in values.iter() {
+            // An error is an error, regardless of fields (for now)
+            assert!(matches!(VerminGrade::parse(v.clone()), Err(InputValueError { .. })));
+        }
+    }
+
+    #[test]
+    fn to_value() {
+        let grades = [VerminGrade(0)];
+        let expected = ["V0"];
+
+        for (g, e) in grades.iter().zip(expected.iter()) {
+            assert_eq!(g.to_value(), Value::String(e.to_string()));
+        }
+    }
+}
