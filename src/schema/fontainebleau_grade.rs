b3f1653d69a2972eac46ebd74ea4ddc96dd0a459
Logan Grosz <logan.grosz@gmail.com>
2025-02-21 16:31:54 -0700

    test: add fontainebleau-grade scalar tests
    These tests cover all lines and ended up exposing a bug where modifiers
    were allowed for grades < 6.

--- src/schema/fontainebleau_grade.rs
+++ src/schema/fontainebleau_grade.rs
@@ -2,7 +2,7 @@ use async_graphql::{Scalar, ScalarType, Value, InputValueError, InputValueResult
 use postgres_types::{FromSql, ToSql};
 use regex::Regex;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct FontainebleauGrade {
     pub number: u8,
     pub letter: Option<FontainebleauLetter>,
@@ -60,3 +60,97 @@ impl ScalarType for FontainebleauGrade {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_good() {
+        let values = [
+            Value::String("F5".into()),
+            Value::String("F5+".into()),
+            Value::String("F6A".into()),
+            Value::String("F6B".into()),
+            Value::String("F6C".into()),
+            Value::String("F7A+".into()),
+        ];
+
+        let expected = [
+            FontainebleauGrade {
+                number: 5,
+                letter: None,
+                plus: false,
+            },
+            FontainebleauGrade {
+                number: 5,
+                letter: None,
+                plus: true,
+            },
+            FontainebleauGrade {
+                number: 6,
+                letter: Some(FontainebleauLetter::A),
+                plus: false,
+            },
+            FontainebleauGrade {
+                number: 6,
+                letter: Some(FontainebleauLetter::B),
+                plus: false,
+            },
+            FontainebleauGrade {
+                number: 6,
+                letter: Some(FontainebleauLetter::C),
+                plus: false,
+            },
+            FontainebleauGrade {
+                number: 7,
+                letter: Some(FontainebleauLetter::A),
+                plus: true,
+            },
+        ];
+
+        for (v, e) in values.iter().zip(expected.iter()) {
+            assert_eq!(FontainebleauGrade::parse(v.clone()).unwrap(), *e);
+        }
+    }
+
+    #[test]
+    fn parse_bad() {
+        let values = [
+            Value::Number(0.into()), // only string-values accepted
+            Value::String("5.10".into()), // doesn't match regex
+            Value::String("F5A".into()), // modifiers only valid for grades >5
+            Value::String("F6".into()), // modifiers required for grades >5
+        ];
+
+        for v in values.iter() {
+            // An error is an error, regardless of fields (for now)
+            assert!(
+                matches!(FontainebleauGrade::parse(v.clone()), Err(InputValueError { .. })),
+                "{} expected to fail parsing but didn't", v
+                );
+        }
+    }
+
+    #[test]
+    fn to_value() {
+        let grades = [
+            FontainebleauGrade { number: 5, letter: None, plus: false },
+            FontainebleauGrade { number: 5, letter: None, plus: true },
+            FontainebleauGrade { number: 6, letter: Some(FontainebleauLetter::A), plus: false },
+            FontainebleauGrade { number: 6, letter: Some(FontainebleauLetter::B), plus: false },
+            FontainebleauGrade { number: 6, letter: Some(FontainebleauLetter::C), plus: false },
+        ];
+
+        let expected = [
+            "F5",
+            "F5+",
+            "F6A",
+            "F6B",
+            "F6C",
+        ];
+
+        for (g, e) in grades.iter().zip(expected.iter()) {
+            assert_eq!(g.to_value(), Value::String(e.to_string()));
+        }
+    }
+}
