6bc8900878e612d1c86130b02f3f0295f6f9882a
Logan Grosz <logan.grosz@gmail.com>
2025-02-21 22:35:17 -0700

    test: add yosemite-decimal-grade scalar tests

--- src/schema/yosemite_decimal_grade.rs
+++ src/schema/yosemite_decimal_grade.rs
@@ -2,7 +2,7 @@ use async_graphql::{Scalar, ScalarType, Value, InputValueError, InputValueResult
 use postgres_types::{FromSql, ToSql};
 use regex::Regex;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct YosemiteDecimalGrade {
     pub grade: u8,
     pub letter: Option<YosemiteDecimalLetter>,
@@ -62,3 +62,97 @@ impl ScalarType for YosemiteDecimalGrade {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_good() {
+        let values = [
+            Value::String("5.9".into()),
+            Value::String("5.10a".into()),
+            Value::String("5.10b".into()),
+            Value::String("5.10c".into()),
+            Value::String("5.10d".into()),
+            Value::String("6".into()),
+            Value::String("11b".into()),
+        ];
+
+        let expected = [
+            YosemiteDecimalGrade {
+                grade: 9,
+                letter: None,
+            },
+            YosemiteDecimalGrade {
+                grade: 10,
+                letter: Some(YosemiteDecimalLetter::A),
+            },
+            YosemiteDecimalGrade {
+                grade: 10,
+                letter: Some(YosemiteDecimalLetter::B),
+            },
+            YosemiteDecimalGrade {
+                grade: 10,
+                letter: Some(YosemiteDecimalLetter::C),
+            },
+            YosemiteDecimalGrade {
+                grade: 10,
+                letter: Some(YosemiteDecimalLetter::D),
+            },
+            YosemiteDecimalGrade {
+                grade: 6,
+                letter: None,
+            },
+            YosemiteDecimalGrade {
+                grade: 11,
+                letter: Some(YosemiteDecimalLetter::B),
+            },
+        ];
+
+        for (v, e) in values.iter().zip(expected.iter()) {
+            assert_eq!(YosemiteDecimalGrade::parse(v.clone()).unwrap(), *e);
+        }
+    }
+
+    #[test]
+    fn parse_bad() {
+        let values = [
+            Value::Number(0.into()), // only string-values accepted
+            Value::String("f8a".into()), // invalid format
+            Value::String("5.0".into()), // grades start a 1
+            Value::String("5.9a".into()), // grades 9 and below must not have a letter
+            Value::String("5.10".into()), // grades 10 and above must have a letter
+        ];
+
+        for v in values.iter() {
+            // An error is an error, regardless of fields (for now)
+            assert!(
+                matches!(YosemiteDecimalGrade::parse(v.clone()), Err(InputValueError { .. })),
+                "{} expected to fail parsing but didn't", v
+                );
+        }
+    }
+
+    #[test]
+    fn to_value() {
+        let grades = [
+            YosemiteDecimalGrade { grade: 9, letter: None },
+            YosemiteDecimalGrade { grade: 10, letter: Some(YosemiteDecimalLetter::A) },
+            YosemiteDecimalGrade { grade: 10, letter: Some(YosemiteDecimalLetter::B) },
+            YosemiteDecimalGrade { grade: 10, letter: Some(YosemiteDecimalLetter::C) },
+            YosemiteDecimalGrade { grade: 10, letter: Some(YosemiteDecimalLetter::D) },
+        ];
+
+        let expected = [
+            "5.9",
+            "5.10a",
+            "5.10b",
+            "5.10c",
+            "5.10d",
+        ];
+
+        for (g, e) in grades.iter().zip(expected.iter()) {
+            assert_eq!(g.to_value(), Value::String(e.to_string()));
+        }
+    }
+}
