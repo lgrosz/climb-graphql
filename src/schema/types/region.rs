f995877ed902e2234320406b7c848c9bfa5ae071
Logan Grosz <logan.grosz@gmail.com>
2025-08-24 20:28:33 -0600

    feat(region): add object type

---
+++ src/schema/types/region.rs
@@ -0,0 +1,30 @@
+use async_graphql::{Context, Object, ID, Result};
+
+use crate::AppData;
+
+pub struct Region(pub i32);
+
+#[Object]
+impl Region {
+    async fn id(&self) -> ID {
+        self.0.into()
+    }
+
+    async fn name(&self, ctx: &Context<'_>) -> Result<Option<String>> {
+        let data = ctx.data::<AppData>()?;
+        let client = match &data.pg_pool {
+            Some(pool) => pool.get().await?,
+            None => {
+                return Err("Database connection is not available".into());
+            }
+        };
+
+        let result = client
+            .query_one("SELECT name FROM climb.regions WHERE id = $1", &[&self.0])
+            .await?;
+        let value: Option<&str> = result.try_get(0)?;
+
+        Ok(value.map(|name| name.to_string()))
+    }
+}
+
