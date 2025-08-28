8fdb4da6ecb95858ee7e2e19cf5cb68c695ea3d3
Logan Grosz <logan.grosz@gmail.com>
2025-08-27 22:08:40 -0600

    feat(region): add formations field

--- src/schema/types/region.rs
+++ src/schema/types/region.rs
@@ -2,7 +2,7 @@ use async_graphql::{Context, Object, ID, Result};
 
 use crate::AppData;
 
-use super::crag::Crag;
+use super::{crag::Crag, formation::Formation};
 
 pub struct Region(pub i32);
 
@@ -46,6 +46,25 @@ impl Region {
             .await?;
 
         Ok(rows.into_iter().map(|row| Crag(row.get(0))).collect())
+    }
+
+    async fn formations(&self, ctx: &Context<'_>) -> Result<Vec<Formation>> {
+        let data = ctx.data::<AppData>()?;
+        let client = match &data.pg_pool {
+            Some(pool) => pool.get().await?,
+            None => {
+                return Err("Database connection is not available".into());
+            }
+        };
+
+        let rows = client
+            .query(
+                "SELECT id FROM climb.formations WHERE region_id = $1 ORDER BY name",
+                &[&self.0],
+            )
+            .await?;
+
+        Ok(rows.into_iter().map(|row| Formation(row.get(0))).collect())
     }
 }
 
