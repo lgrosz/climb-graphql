38d721f889d85446809df7d21ed8c97e7cc1ccbb
Logan Grosz <logan.grosz@gmail.com>
2025-09-07 22:02:02 -0600

    feat(sector): add description field

--- src/schema/types/sector.rs
+++ src/schema/types/sector.rs
@@ -29,6 +29,23 @@ impl Sector {
         Ok(value.map(|name| name.to_string()))
     }
 
+    async fn description(&self, ctx: &Context<'_>) -> Result<Option<String>> {
+        let data = ctx.data::<AppData>()?;
+        let client = match &data.pg_pool {
+            Some(pool) => pool.get().await?,
+            None => {
+                return Err("Database connection is not available".into());
+            }
+        };
+
+        let result = client
+            .query_one("SELECT description FROM climb.sectors WHERE id = $1", &[&self.0])
+            .await?;
+        let value: Option<&str> = result.try_get(0)?;
+
+        Ok(value.map(|s| s.to_string()))
+    }
+
     async fn crag(&self, ctx: &Context<'_>) -> Result<Option<Crag>> {
         let data = ctx.data::<AppData>()?;
         let client = match &data.pg_pool {
