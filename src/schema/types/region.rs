95690fd6a3ccb9ebbe3886aeb35e6e070b43875b
Logan Grosz <logan.grosz@gmail.com>
2025-09-07 22:01:45 -0600

    feat(region): add description field

--- src/schema/types/region.rs
+++ src/schema/types/region.rs
@@ -29,6 +29,23 @@ impl Region {
         Ok(value.map(|name| name.to_string()))
     }
 
+    async fn description(&self, ctx: &Context<'_>) -> Result<Option<String>> {
+        let data = ctx.data::<AppData>()?;
+        let client = match &data.pg_pool {
+            Some(pool) => pool.get().await?,
+            None => {
+                return Err("Database connection is not available".into());
+            }
+        };
+
+        let result = client
+            .query_one("SELECT description FROM climb.regions WHERE id = $1", &[&self.0])
+            .await?;
+        let value: Option<&str> = result.try_get(0)?;
+
+        Ok(value.map(|s| s.to_string()))
+    }
+
     async fn crags(&self, ctx: &Context<'_>) -> Result<Vec<Crag>> {
         let data = ctx.data::<AppData>()?;
         let client = match &data.pg_pool {
